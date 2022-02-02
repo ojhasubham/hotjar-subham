8f6f4510fa5ec9732bb57964373dde6f4b1a5484
subham-o-trustclarity <subham.o@trustclarity.com>
2022-02-02 15:04:28 +0530

    add ads google code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Hotjar Demo....</h3>
+        <h3>google ads</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
